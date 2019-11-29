a6a0e7666a86ea265aee6404cadb3f8d661f0ec1
Chhaian Pin <chhaianpin@hotmail.com>
2019-11-28 21:52:49 -0800

    completed largest numbers in array challenge

---
+++ LargestOfFour.js
@@ -0,0 +1,19 @@
+const largestOfFour = (arr) => {
+    const newArr = [];
+    for (let arr1 of arr) {
+        let max = Number.MIN_SAFE_INTEGER;
+        for (let num of arr1) {
+            if (num > max) {
+                max = num;
+            }
+        }
+        newArr.push(max);
+    }
+
+    return newArr;
+}
+
+console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
+console.log(largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
+console.log(largestOfFour([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]]));
+console.log(largestOfFour([[17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -17, -10]]));
