ef7a827adf8e1a1ac52e7a96ae716b09eb9f5e0c
Chhaian Pin <chhaianpin@hotmail.com>
2019-11-28 21:06:01 -0800

    staring a JavaScript practice space from FreeCodeCamp.org resources

---
+++ AlgorithmScripting.js
@@ -0,0 +1,9 @@
+function convertToF(celsius) {
+    let fahrenheit = celsius * (9 / 5) + 32;
+    return fahrenheit;
+}
+
+console.log(convertToF(30));
+console.log(convertToF(0));
+console.log(convertToF(-30));
+// console.log("hello world");
