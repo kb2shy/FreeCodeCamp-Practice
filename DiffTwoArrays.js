8986a57de7f1b09975a0091568cc15c9ced251b6
Chhaian Pin <chhaianpin@hotmail.com>
2019-12-03 09:05:53 -0800

    completed return the elements that different between the two arrays

---
+++ DiffTwoArrays.js
@@ -0,0 +1,23 @@
+const diffTwoArrays = (arr1, arr2) => {
+    const newArr = [];
+    for (let el of arr1) {
+        if (!arr2.includes(el) && !newArr.includes(el)) {
+            newArr.push(el);
+        }
+    }
+
+    for (let el of arr2) {
+        if (!arr1.includes(el) && !newArr.includes(el)) {
+            newArr.push(el);
+        }
+    }
+
+    return newArr;
+}
+
+console.log(diffTwoArrays([1, 2, 3, 5], [1, 2, 3, 4, 5]));
+console.log(diffTwoArrays(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]));
+console.log(diffTwoArrays(["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]));
+console.log(diffTwoArrays(["andesite", "grass", "dirt", "dead shrub"], ["andesite", "grass", "dirt", "dead shrub"]));
+console.log(diffTwoArrays([1, "calf", 3, "piglet"], [1, "calf", 3, 4]));
+console.log(diffTwoArrays([], ["snuffleupagus", "cookie monster", "elmo"]));
