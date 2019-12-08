2d7fe8441318c20f1cba55ceb5185a05c6193abe
Chhaian Pin <chhaianpin@hotmail.com>
2019-12-07 21:10:46 -0800

    found 2nd way to solve sumOddFibNumbers.js challenge

--- SumOddFibNumbers.js
+++ SumOddFibNumbers.js
@@ -1,22 +1,33 @@
 const sumFibs = (num) => {
-    const fibArr = [1, 1];
-    let current = 0;
-    let lastIndex = 1;
+    // const fibArr = [1, 1];
+    // let current = 0;
+    // let lastIndex = 1;
 
-    while (current <= num) {
-        current = fibArr[lastIndex - 1] + fibArr[lastIndex];
-        fibArr.push(current);
-        lastIndex++;
-    }
+    // while (current <= num) {
+    //     current = fibArr[lastIndex - 1] + fibArr[lastIndex];
+    //     fibArr.push(current);
+    //     lastIndex++;
+    // }
 
-    let sum = 0;
-    for (let el of fibArr) {
-        if (el % 2 === 1 && el <= num) {
-            sum += el;
+    // let sum = 0;
+    // for (let el of fibArr) {
+    //     if (el % 2 === 1 && el <= num) {
+    //         sum += el;
+    //     }
+    // }
+    // return sum;
+
+    let prev = 0, curr = 1, result = 0;
+    while (curr <= num) {
+        if (curr % 2 !== 0) {
+            result += curr;
         }
-    }
-    return sum;
 
+        curr += prev;
+        prev = curr - prev;
+    }
+
+    return result;
 }
 
 // const fib = (num) => {
