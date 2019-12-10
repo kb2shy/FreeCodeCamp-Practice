d8b3a7638ed1cf5d9eaa17f192a3f13b19cdb70c
Chhaian Pin <chhaianpin@hotmail.com>
2019-12-09 17:25:03 -0800

    completed Map The Debris challenge and all challenges from FreeCodeCamp Intermediate Algorithm section

---
+++ MapTheDebris.js
@@ -0,0 +1,17 @@
+const orbitalPeriod = (arr) => {
+    const GM = 398600.4418;
+    const earthRadius = 6367.4447
+
+    const newArr = [];
+    arr.forEach(obj => {
+        const a = Math.pow(earthRadius + obj["avgAlt"], 3);
+        const op = 2 * Math.PI * Math.sqrt(a / GM);
+        newArr.push({name: obj["name"], orbitalPeriod: Math.round(op)});
+    });
+    // const a = Math.pow(earthRadius + arr[0]["avgAlt"], 3);
+    // const op = 2 * Math.PI * Math.sqrt(a / GM);
+    // newArr.push({ orbitalPeriod: Math.round(op)})
+    return newArr;
+}
+
+console.log(orbitalPeriod([{name: "sputnik", avgAlt: 35873.5553}]));
