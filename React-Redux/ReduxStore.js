7f9f3e4ec589416aa4718bea7c90df3db1635d5d
Chhaian Pin <chhaianpin@hotmail.com>
2019-12-23 09:36:36 -0800

    created a redux action, defined action creator, and dispatched action

--- React-Redux/ReduxStore.js
+++ React-Redux/ReduxStore.js
@@ -3,3 +3,19 @@ const reducer = (state = 5) => {
 }
 
 const store = Redux.createStore(reducer);
+
+let currentState = store.getState();
+
+// defining a Redux action
+const action = {
+    type: "LOGIN"
+}
+
+// Defining an action creator
+const actionCreator = () => {
+    return action;
+}
+
+// Dispatch the action
+store.dispatch(actionCreator());
+store.dispatch({type: 'LOGIN'});
